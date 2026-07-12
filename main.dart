7c855dd1d7586ca1ddb2b6bc4c0d34aba96beab7
Shehab Mtwan <shehabmtwan@gmail.com>
2026-07-12 17:52:00 +0300

    Add class jwallyMethod

--- main.dart
+++ main.dart
@@ -3,3 +3,10 @@ void main() {}
 abstract class paymentMethod {
   bool pay(double amount);
 }
+class jwallyMethod implements paymentMethod {
+  @override
+  bool pay(double amount) {
+    print('Paying $amount using Jwally');
+    return true;
+  }
+}
